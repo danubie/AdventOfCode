b9c5bd69087e6f192c1b077524b4fe823332ca51
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2022-12-03 18:02:41 +0100

    refactor to prepare for Part 2

--- 2022/Day03-RucksackReorganization/Day03.ps1
+++ 2022/Day03-RucksackReorganization/Day03.ps1
@@ -1,17 +1,31 @@
-# if $Item is found in $ItemList returns
+# if $Item is found in any of the strings in $ItemLists returns
 #   Item, SValue
-function Find-Item {
+function Get-ItemValue {
     [CmdletBinding()]
     param (
-        [string] $Item,
-        [string] $ItemList
+        [string[]] $ItemsToFind,
+        [string[]] $ItemLists
     )
 
-    if ($ItemList -match $Item) {
-        $Item, $SValue = $Matches[1], $Matches[2]
-        return $Item, $SValue
+    $sum = 0
+    foreach ($c in $ItemsToFind) {
+        $foundInAll = $true
+        foreach ($list in $ItemLists) {
+            $foundInAll = $foundInAll -and ($list -cmatch $c)
+        }
+        if (-Not $foundInAll) {
+            continue
+        }
+        # if ($c -eq '') { continue }         # skip empty char at top of the list
+        if ([byte][char] $c -gt [byte][char]'a' ) {
+            $value = [byte][char]$c - [byte][char]'a' + 1
+        } else {
+            $value = [byte][char]$c -[byte][char]'A' + 27
+        }
+        $sum += $value
+        Write-Verbose "Foundx $c with value $value in $comp2 => sum = $sum"
     }
-
+    $sum
 }
 
 function Day03 {
@@ -22,24 +36,20 @@ function Day03 {
 
     $data = Get-Content -Path $InputFile
     $sum = 0
+    $sum2 = 0
     foreach ($line in $data) {
         # cut the rucksack into it's 2 compartments
         $comp1 = $line.Substring(0, $line.Length/2)
         $comp2 = $line.Substring($line.Length/2, $line.Length/2)
+
         # find each item (=character) of comp1 in comp2
         $ItemsToFind = $comp1 -split '' | Select-Object -Unique | Where-Object { $_ -ne '' }
-        foreach ($c in $ItemsToFind) {
-            # if ($c -eq '') { continue }         # skip empty char at top of the list
-            if ($comp2 -cmatch $c) {
-                if ([byte][char] $c -gt [byte][char]'a' ) {
-                    $value = [byte][char]$c - [byte][char]'a' + 1
-                } else {
-                    $value = [byte][char]$c -[byte][char]'A' + 27
-                }
-                $sum += $value
-                Write-Verbose "Found $c with value $value in $comp2 => sum = $sum"
-            }
+        $value2 = Get-ItemValue -ItemsToFind $ItemsToFind -ItemLists $comp2
+        if ($value2 -ne 0) {
+            Write-Verbose "Found2 $ItemsToFind with value $value2 in $comp2 => sum = $sum2"
         }
+
+        $sum2 += $value2
     }
-    $sum
+    $sum2
 }
