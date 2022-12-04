d1b2cff59084b6e60bf0b977c618837a2ae5e2b3
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2022-12-04 08:38:57 +0100

    Day 4 Part 1

---
+++ 2022/Day04-CampCleanup/Day04.Tests.ps1
@@ -0,0 +1,28 @@
+BeforeAll {
+    . $PSScriptRoot\Day04.ps1
+}
+
+Describe 'Testing' {
+    BeforeAll {
+        Mock Get-Content { @(
+            '2-4,6-8'
+            '2-3,4-5'
+            '5-7,7-9'
+            '2-8,3-7'
+            '6-6,4-6'
+            '2-6,4-8'
+            )
+        }
+    }
+    It 'Part 1 should return 2' {
+        $result = Day04 -Input $PSScriptRoot/InputData.txt
+        $result | Where FullyContains -eq $true | Should -HaveCount 2
+    }
+}
+Describe 'Real Data' {
+    It 'Part 1 should return 475' {
+        $result = Day04 -Input $PSScriptRoot/InputData.txt
+        $result | Where FullyContains -eq $true | Should -HaveCount 475
+    }
+
+}
---
+++ 2022/Day04-CampCleanup/Day04.ps1
@@ -0,0 +1,26 @@
+function Day04 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $InputFile
+    )
+
+    $data = Get-Content -Path $InputFile
+    foreach ($line in $data) {
+        $pairInLine = $line -split ','
+        $pair = [PSCustomObject]@{
+            FullyContains = $false
+            Section1 = $null
+            Section2 = $null
+        }
+        $borders = $pairInLine[0] -split '-'
+        $pair.Section1 = [pscustomobject]@{ lbound = [int]$borders[0]; ubound = [int]$borders[1] }
+        $borders = $pairInLine[1] -split '-'
+        $pair.Section2 = [pscustomobject]@{ lbound = [int]$borders[0]; ubound = [int]$borders[1] }
+        $pair.FullyContains = ($pair.Section1.lbound -le $pair.Section2.lbound) -and ($pair.Section1.ubound -ge $pair.Section2.ubound)
+        $pair.FullyContains = $pair.FullyContains -or (($pair.Section2.lbound -le $pair.Section1.lbound) -and ($pair.Section2.ubound -ge $pair.Section1.ubound))
+        $pair
+
+        # True @{lbound=53; ubound=92} @{lbound=6; ubound=52}
+    }
+}
