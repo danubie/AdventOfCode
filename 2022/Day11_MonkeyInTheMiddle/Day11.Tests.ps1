ce909552314b4d82f2be653f67d1c17376709a98
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2022-12-11 13:59:52 +0100

    Part 1 finished, Part 2 still something wrong

---
+++ 2022/Day11_MonkeyInTheMiddle/Day11.Tests.ps1
@@ -0,0 +1,150 @@
+# Day11
+BeforeAll {
+    . $PSScriptRoot\Day11.ps1
+}
+
+Describe 'Testdata' {
+    Beforeall {
+        Mock Get-Content {@(
+  '  Monkey 0:'
+  '  Starting items: 79, 98'
+  '  Operation: new = old * 19'
+  '  Test: divisible by 23'
+  '    If true: throw to monkey 2'
+  '    If false: throw to monkey 3'
+'  '
+  'Monkey 1:'
+  '  Starting items: 54, 65, 75, 74'
+  '  Operation: new = old + 6'
+  '  Test: divisible by 19'
+  '    If true: throw to monkey 2'
+  '    If false: throw to monkey 0'
+'  '
+  'Monkey 2:'
+  '  Starting items: 79, 60, 97'
+  '  Operation: new = old * old'
+  '  Test: divisible by 13'
+  '    If true: throw to monkey 1'
+  '    If false: throw to monkey 3'
+'  '
+  'Monkey 3:'
+  '  Starting items: 74'
+  '  Operation: new = old + 3'
+  '  Test: divisible by 17'
+  '    If true: throw to monkey 0'
+  '    If false: throw to monkey 1'
+        )
+        }
+    }
+    Context 'Part 1' {
+        It 'should return correct result for round 1' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 1
+                Verbose   = $false
+            }
+            $result = Day11 @SplatDay11
+            $Script:Monkeys[0].WorryLevels.ToArray() | Should -Be (20, 23, 27, 26)
+            $Script:Monkeys[1].WorryLevels.ToArray() | Should -Be (2080, 25, 167, 207, 401, 1046)
+            $Script:Monkeys[2].WorryLevels.Count | Should -Be 0
+            $Script:Monkeys[3].WorryLevels.Count | Should -Be 0
+        }
+        It 'should return correct result for 20 rounds' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 20
+                Verbose   = $false
+            }
+            $result = Day11 @SplatDay11
+            $Script:Monkeys[0].WorryLevels.ToArray() | Should -Be (10, 12, 14, 26, 34)
+            $Script:Monkeys[1].WorryLevels.ToArray() | Should -Be (245, 93, 53, 199, 115)
+            $Script:Monkeys[2].WorryLevels.Count | Should -Be 0
+            $Script:Monkeys[3].WorryLevels.Count | Should -Be 0
+
+            $Script:Monkeys[0].NbInspections | Should -Be 101
+            $Script:Monkeys[1].NbInspections | Should -Be 95
+            $Script:Monkeys[2].NbInspections | Should -Be 7
+            $Script:Monkeys[3].NbInspections | Should -Be 105
+
+            $Result.Part1 | Should -Be 10605
+        }
+    }
+    Context 'Part 2' {
+        It 'should return correct result for 1 rounds' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 1
+                Part2     = $true
+                Verbose   = $true
+            }
+            $result = Day11 @SplatDay11
+
+            $Script:Monkeys[0].NbInspections | Should -Be 2
+            $Script:Monkeys[1].NbInspections | Should -Be 4
+            $Script:Monkeys[2].NbInspections | Should -Be 3
+            $Script:Monkeys[3].NbInspections | Should -Be 6
+        }
+        It 'should return correct result for 20 rounds' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 20
+                Part2     = $true
+                Verbose   = $true
+            }
+            $result = Day11 @SplatDay11
+
+            $Script:Monkeys[0].NbInspections | Should -Be 99
+            $Script:Monkeys[1].NbInspections | Should -Be 97
+            $Script:Monkeys[2].NbInspections | Should -Be 8
+            $Script:Monkeys[3].NbInspections | Should -Be 103
+        }
+        It 'should return correct result for 1000 rounds' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 1000
+                Part2     = $true
+                Verbose   = $false
+            }
+            $result = Day11 @SplatDay11
+
+            $Script:Monkeys[0].NbInspections | Should -Be 5204
+            $Script:Monkeys[1].NbInspections | Should -Be 4792
+            $Script:Monkeys[2].NbInspections | Should -Be 199
+            $Script:Monkeys[3].NbInspections | Should -Be 5192
+
+            $Result.Part2 | Should -Be 2713310158
+        }
+
+    }
+}
+Describe 'Real data' {
+    Context 'Part 1' {
+        It 'should return correct result for 20 rounds' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 20
+                Verbose   = $false
+            }
+            $result = Day11 @SplatDay11
+            $Result.Part1 | Should -Be 54036
+        }
+    }
+    Context 'Part 2' {
+        It 'should return correct result for 1000 rounds' {
+            $Script:Monkeys = [System.Collections.ArrayList]::new()
+            $splatDay11 = @{
+                InputFile = "$PSScriptRoot\inputdata.txt"
+                Rounds    = 1000
+                Verbose   = $false
+            }
+            $result = Day11 @SplatDay11
+            $Result.Part2 | Should -Be 2713310158
+        }
+    }
+}
---
+++ 2022/Day11_MonkeyInTheMiddle/Day11.ps1
@@ -0,0 +1,122 @@
+$Script:monkeys = [System.Collections.ArrayList]::new()
+
+function Print-Monkey {
+    [CmdletBinding()]
+    param (
+        [int] $Nr,
+        $msg
+    )
+
+    $m = $Script:monkeys[$nr]
+    Write-Verbose "$msg Monkey $Nr, $($m.WorryLevels.Count) items, [$($m.WorryLevels.ToArray())]"
+}
+
+function Add-Monkey {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string[]] $monkeyData
+    )
+    Write-Verbose "Add-Monkey: $monkeyData"
+    $splat = [pscustomobject][ordered]@{
+        Nr             = ([int] ($monkeyData[0] -replace "Monkey (\d+):", '$1'))
+        Operation      = 'unknown'
+        OperationValue = 0
+        TestValue      = ([int] ($monkeyData[3] -replace "Test: divisible by (\d+)", '$1'))
+        ToMonkeyTrue   = ([int] ($monkeyData[4] -replace "If true: throw to monkey (\d+)", '$1'))
+        ToMonkeyFalse  = ([int] ($monkeyData[5] -replace "If false: throw to monkey (\d+)", '$1'))
+        WorryLevels    = [System.Collections.Queue]::new()
+        NbInspections  = 0
+    }
+    $isMatch = $monkeyData[2] -match "Operation: new = old (?<opcode>\S) (?<value>\S+)"
+    if (!$isMatch) { Throw "Operation not matched"}
+    $splat.Operation = $Matches.opcode
+    $splat.OperationValue = $Matches.value
+    # woory levels converted to int[] and then into Queue
+    [int64[]]($monkeyData[1] -replace "(  Starting items: )(.*)", '$2' -split ', ') | ForEach-Object { $splat.WorryLevels.Enqueue($_) }
+
+    $index = $Script:monkeys.Add($splat)
+    Write-Verbose "Added monkey $index; $($splat)"
+}
+
+function Import-Monkeys {
+    [CmdletBinding()]
+    param (
+        [string] $InputFile
+    )
+    $data = Get-Content -Path $InputFile
+    for ($i = 0; $i -lt $data.Count; ) {
+        # read in group of 6
+        $inputBlock = $data[$i..($i+5)]
+        Add-Monkey $inputBlock
+        # skip 1 line
+        $i += 7
+    }
+}
+
+function Invoke-MonkeyThrows {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        $monkey,
+        [switch] $Part2
+    )
+    Write-Verbose "Monkey $($monkey.Nr) items: $($monkey.WorryLevels.ToArray()) "
+    $StartCount = $monkey.WorryLevels.Count
+    for ($i=0; $i -le $StartCount-1; $i++) {
+        $monkey.NbInspections++
+        $itemLevel = $monkey.WorryLevels.Dequeue()
+        if ($monkey.OperationValue -eq 'old') {
+            $currOperationValue = $itemLevel
+        } else {
+            $currOperationValue = $monkey.OperationValue
+        }
+        switch ($monkey.Operation) {
+            '+' { $newLevel = $itemlevel + $currOperationValue }
+            '-' { $newLevel = $itemlevel - $currOperationValue }
+            '*' { $newLevel = $itemlevel * $currOperationValue }
+            Default { Throw "Unknown operation $($monkey.Operation)" }
+        }
+        if (!$Part2) {
+            $newLevel = [System.Math]::Floor($newLevel / 3)
+        }
+        if ($newlevel % $monkey.TestValue -eq 0) {
+            $throwingTo = $monkey.ToMonkeyTrue
+        } else {
+            $throwingTo = $monkey.ToMonkeyFalse
+        }
+        Write-Verbose "    $itemLevel -> $newLevel -> throwing to $throwingTo"
+        $Script:monkeys[$throwingTo].WorryLevels.Enqueue($newLevel)
+        Print-Monkey -Nr $throwingTo -Msg "     "
+    }
+}
+
+function Day11 {
+    [CmdletBinding()]
+    param (
+        [string] $InputFile,
+        [int] $Rounds,
+        [switch] $Part2
+    )
+    Import-Monkeys -InputFile $InputFile
+    for ($i=0; $i -lt $Rounds; $i++) {
+        Write-Verbose "Round $($i+1)"
+        foreach ($monkey in $Script:monkeys) {
+            Invoke-MonkeyThrows -monkey $monkey -Part2:$Part2
+        }
+        Write-Verbose "These are the intermediate results:"
+        $Script:monkeys
+            | ForEach-Object { Print-Monkey -Nr $_.Nr -Msg "     " }
+    }
+    # calc Part 1 result
+    $Multiplier = 1
+    $Script:monkeys.NbInspections
+    | Sort-Object -Descending
+    | Select-Object -First 2
+    | ForEach-Object { $Multiplier *= $_ }
+    $result = [PSCustomObject]@{
+        Part1 = $Multiplier
+    }
+    $result
+}
+
