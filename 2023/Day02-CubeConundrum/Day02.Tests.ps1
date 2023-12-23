a82324a3bb5b743ace86d196e3a15745a1747285
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2023-12-23 13:00:49 +0100

    Day02 Part 2

--- 2023/Day02-CubeConundrum/Day02.Tests.ps1
+++ 2023/Day02-CubeConundrum/Day02.Tests.ps1
@@ -46,3 +46,37 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         }
     }
 }
+
+Describe "Part 2" {
+    Context "Basic tests" {
+
+        It "<gameline> should return <expected>" -ForEach @(
+            @{ expected = 48    ; gameline = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green' }
+            @{ expected = 12    ; gameline = 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue' }
+            @{ expected = 1560  ; gameline = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red' }
+            @{ expected = 630   ; gameline = 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red' }
+            @{ expected = 36    ; gameline = 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green' }
+            ) {
+                Get-PowerOfCubeSet -gameRecord $gameline | Should -Be $expected
+            }
+        It "should return sum of valid game indexes" {
+    $testdata = @'
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+'@
+            $testdata | Out-File "$PSScriptRoot/Testdata.txt"
+            $result = Day02 -InputFile "$PSScriptRoot/Testdata.txt" -Part2
+            $result | Should -Be 2286
+            Remove-Item "$PSScriptRoot/Testdata.txt"
+        }
+    }
+    Context "Solution tests" {
+        It "should return correct value" {
+            $result = Day02 -InputFile "$PSScriptRoot/Input.txt" -Part2
+            $result | Should -Be 56580
+        }
+    }
+}
--- 2023/Day02-CubeConundrum/Day02.ps1
+++ 2023/Day02-CubeConundrum/Day02.ps1
@@ -1,3 +1,35 @@
+function Get-IndexAndCubeListFromRecord {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string] $GameRecord
+    )
+
+    begin {
+
+    }
+
+    process {
+        # Split the game record into it's components by regex
+        # "Game <index>: list of number and color joined by a semicolon"
+        $null = $GameRecord -match "Game (?<index>\d+): (?<cubes>.+)"
+        $gameIndex = $Matches["index"]
+        $gameCubes = $Matches["cubes"]
+        # use regex to split $gameCubes first by semicolon, then by space. return the number of cubes in each color
+        $regex = [regex] '\s*(?<count>\d+)\s+(?<color>[^;,]+)'
+        $cubesInGame = $regex.Matches($gameCubes) | ForEach-Object {
+            [pscustomobject]@{
+                Count = [int]$_.Groups["count"].Value
+                Color = $_.Groups["color"].Value
+            }
+        }
+        $gameIndex, $cubesInGame
+    }
+
+    end {
+
+    }
+}
 function Get-GameIndexIfIsValid {
     [CmdletBinding()]
     param (
@@ -13,19 +45,7 @@ function Get-GameIndexIfIsValid {
     }
 
     process {
-        # Split the game record into it's components by regex
-        # "Game <index>: list of number and color joined by a semicolon"
-        $null = $gameRecord -match "Game (?<index>\d+): (?<cubes>.+)"
-        $gameIndex = $Matches["index"]
-        $gameCubes = $Matches["cubes"]
-        # use regex to split $gameCubes first by semicolon, then by space. return the number of cubes in each color
-        $regex = [regex] '\s*(?<count>\d+)\s+(?<color>[^;,]+)'
-        $cubesInGame = $regex.Matches($gameCubes) | ForEach-Object {
-            [pscustomobject]@{
-                Count = [int]$_.Groups["count"].Value
-                Color = $_.Groups["color"].Value
-            }
-        }
+        $gameIndex, $cubesInGame = Get-IndexAndCubeListFromRecord -GameRecord $gameRecord
         $result = $true
         foreach ($cube in $cubesInGame) {
             if ($cube.Count -gt $cubesInBag[$cube.Color]) {
@@ -43,14 +63,39 @@ function Get-GameIndexIfIsValid {
     }
 }
 
+function Get-PowerOfCubeSet {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string] $GameRecord
+    )
+    begin {}
+    process {
+        $gameIndex, $cubesInGame = Get-IndexAndCubeListFromRecord -GameRecord $GameRecord
+        $maxCountOfEachColors = $cubesInGame | Group-Object -Property Color | ForEach-Object {
+            # return the maximum count of this group
+            $_.Group | Sort-Object -Property Count | Select-Object -Last 1
+        }
+        $result = 1
+        $maxCountOfEachColors | ForEach-Object { $result = $_.Count * $result }
+        $result
+    }
+    end {}
+}
+
 function Day02 {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
         [hashtable] $cubesInBag,
         [string] $InputFile = "$PSScriptRoot/../Input.txt",
         [switch] $Part2
     )
-    $result = Get-Content $InputFile | Get-GameIndexIfIsValid -cubesInBag $cubesInBag -Part2:$Part2 | Measure-Object -Sum
-    $result.Sum
+    if ($Part2) {
+        $result = Get-Content $InputFile | Get-PowerOfCubeSet | Measure-Object -Sum
+        $result.Sum
+    } else {
+        if ($cubesInBag -eq $null) { Throw "CubesInBag must be specified for part 1" }
+        $result = Get-Content $InputFile | Get-GameIndexIfIsValid -cubesInBag $cubesInBag -Part2:$Part2 | Measure-Object -Sum
+        $result.Sum
+    }
 }
