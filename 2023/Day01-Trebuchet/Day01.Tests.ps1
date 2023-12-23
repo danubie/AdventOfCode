fb5bf8a22fcc80d0b191b6120e6754c7159f2b19
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2023-12-23 11:38:02 +0100

    Solving part 2

--- 2023/Day01-Trebuchet/Day01.Tests.ps1
+++ 2023/Day01-Trebuchet/Day01.Tests.ps1
@@ -34,3 +34,39 @@ Describe "Solution tests" {
         Day01 -InputFile "$PSScriptRoot/Input.txt" | Should -Be 54990.
     }
 }
+
+Describe "Part 2" {
+    Context "Basic tests" {
+        It "'<toTest>' should return <expected>" -ForEach @(
+            @{ toTest = "two1nine"           ; Expected = 29 }
+            @{ toTest = "eightwothree"       ; Expected = 83 }
+            @{ toTest = "abcone2threexyz"    ; Expected = 13 }
+            @{ toTest = "xtwone3four"        ; Expected = 24 }
+            @{ toTest = "4nineeightseven2"   ; Expected = 42 }
+            @{ toTest = "zoneight234"        ; Expected = 14 }
+            @{ toTest = "7pqrstsixteen"      ; Expected = 76 }
+        ) {
+            Get-CalibrationValue -InputObject $toTest -Part2 | Should -Be $expected
+        }
+
+        It "should return 251" {
+            $testdata = @'
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+'@
+                    $testdata | Out-File "$PSScriptRoot/Testdata.txt"
+                    Day01 -InputFile "$PSScriptRoot/Testdata.txt" -Part2 | Should -Be 281
+                    Remove-Item "$PSScriptRoot/Testdata.txt"
+                }
+    }
+    Context "Solution tests" {
+        It "should return correct value" {
+            Day01 -InputFile "$PSScriptRoot/Input.txt" -Part2 | Should -Be 54473
+        }
+    }
+}
