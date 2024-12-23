ea73ea93897fd44ccc6c25de743291cf26e4645a
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2024-12-23 06:33:48 +0100

    check returns array or index of the failed index

--- 2024/Day02.ps1
+++ 2024/Day02.ps1
@@ -8,6 +8,31 @@ function ReadInputData {
         (,$array)
     }
 }
+
+function CheckReport {
+    [CmdletBinding()]
+    param (
+        $Report
+    )
+    $fail = -1
+    if ($report[0] -gt $report[1]) {
+        # it must be a descending list
+        foreach ($i in 1..($report.Length - 1)) {
+            # it must be a descending list and the max difference allowed is 3
+            if ($report[$i] -ge $report[$i - 1]) {$fail = $i; break }
+            if ($report[$i - 1] - $report[$i] -gt 3) {$fail = $i; break }
+        }
+    } else {
+        # it must be an ascending list
+        foreach ($i in 1..($report.Length - 1)) {
+            # it must be an ascending list and the max difference allowed is 3
+            if ($report[$i] -le $report[$i - 1]) {$fail = $i; break }
+            if ($report[$i] - $report[$i - 1] -gt 3) {$fail = $i; break }
+        }
+    }
+    if ($fail -eq -1) { (,$report) }
+    $fail
+}
 function Day02 {
     [CmdletBinding()]
     param (
@@ -17,24 +42,8 @@ function Day02 {
     $reportList = ReadInputData -InputFile $InputFile
         # for each report: check if the sorted list is the same as the original list
     $validReports = foreach ($report in $reportList) {
-        $fail = $false
-        if ($report[0] -gt $report[1]) {
-            # it must be a descending list
-            foreach ($i in 1..($report.Length - 1)) {
-                # it must be a descending list and the max difference allowed is 3
-                if ($report[$i] -ge $report[$i - 1]) { $fail = $true; break }
-                if ($report[$i - 1] - $report[$i] -gt 3) { $fail = $true; break }
-            }
-            if (-not $fail) { (,$report) }
-        } else {
-            # it must be an ascending list
-            foreach ($i in 1..($report.Length - 1)) {
-                # it must be an ascending list and the max difference allowed is 3
-                if ($report[$i] -le $report[$i - 1]) { $fail = $true; break }
-                if ($report[$i] - $report[$i - 1] -gt 3) { $fail = $true; break }
-            }
-            if (-not $fail) { (,$report) }
-        }
+        $result = CheckReport -Report $report
+        if ($result -isnot [int]) { (,$report) }
     }
     $validReports.Count
 }
