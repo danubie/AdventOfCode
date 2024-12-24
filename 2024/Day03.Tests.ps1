c126700ab33ae2b82b4d182c42134937aceced3c
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2024-12-24 06:53:24 +0100

    Part 2

--- 2024/Day03.Tests.ps1
+++ 2024/Day03.Tests.ps1
@@ -31,26 +31,16 @@ Describe 'Part 2' {
         AfterAll {
             if (Test-Path $pathTestdata) { Remove-Item $pathTestdata }
         }
-        It 'should return 4' {
-            Set-Content -Path $pathTestdata -Value $Testdata "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-            $result = Day03 -InputFile $pathTestdata -Part2
+        It 'should return correct result' {
+            Set-Content -Path $pathTestdata -Value "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+            $result = Day03b -InputFile $pathTestdata
             $result | Should -Be 48
         }
-        # It 'sample data <TestCase>' -ForEach @(
-        #     # @{ TestCase = '1';  Expected = 1; Data = "1 2 3 4 5" }
-        #     @{ TestCase = '2';  Expected = 1; Data = '22 25 27 28 30 31 32 29' } #last
-        #     @{ TestCase = '3';  Expected = 1; Data = '5 3 4 5 6' }  #first
-        #     @{ TestCase = '4';  Expected = 1; Data = '72 74 75 77 80 81 81' } #first 81
-        # ) {
-        #     Set-Content -Path $pathTestdata -Value $PSItem.Data
-        #     $result = Day03 -InputFile $pathTestdata -Part2
-        #     $result | Should -Be $PSItem.Expected
-        # }
     }
     Context 'real data' {
-        It '651 is too low' -Skip {
-            $result = Day03 -InputFile "$PSScriptRoot/Data/03.txt" -Part2
-            $result | Should -Be 674
+        It 'should return correct result' {
+            $result = Day03b -InputFile "$PSScriptRoot/Data/03.txt"
+            $result | Should -Be 107069718
         }
     }
 }
--- 2024/Day03.ps1
+++ 2024/Day03.ps1
@@ -11,3 +11,27 @@ function Day03 {
     }
     $sum
 }
+
+function Day03b {
+    [CmdletBinding()]
+    param (
+        [string] $InputFile
+    )
+    $s = Get-Content -Path $InputFile
+    $regex = [regex]::new("((?<op>mul)\((?<x>\d+),(?<y>\d+)\))|(?<disable>don\'t())|(?<enable>do())")
+    $sum = 0
+    $enabled = $true
+    $regex.Matches($s) | ForEach-Object {
+        if ($_.Groups["enable"].Success) {
+            $enabled = $true
+            return
+        }
+        if ($_.Groups["disable"].Success) {
+            $enabled = $false
+            return
+        }
+        if ($enabled -eq $false) { return }
+        $sum += [int]$_.Groups["x"].Value * [int]$_.Groups["y"].Value
+    }
+    $sum
+}
